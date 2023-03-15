a2de35b1eaf559a0f014ebb983c21c5edb90d23f
Leo <223lmaus@frhsd.com>
2023-03-15 11:02:17 -0400

    number 3

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
+// const app = Vue.createApp({
+//     data() {
+//         return {
+//             showbooks: 'true',
+//             title: 'The final empire',
+//             author: 'brandon Sanderson',
+//             age: 45,
+//             x: 0,
+//             y: 0
+//         }
+//     },
+//     methods: {
+//         toggleShowbooks() {
+//             this.showbooks = !this.showbooks
+//         },
+//         handleEvent(e, data) {
+//             console.log(e, e.type)
+//             if (data) {
+//                 console.log(data)
+//             }
+//         },
+//         handleMousemove(e) {
+//             this.x = e.offsetX
+//             this.y = e.offsetY
+//         }
+//     }
+// })
+
+// app.mount('#app')
+
 const app = Vue.createApp({
     data() {
         return {
-            showbooks: true,
-            title: 'The final empire',
-            author: 'brandon Sanderson',
-            age: 45
+            showbooks: 'true',
+            books: [
+                {title: 'name of the wind' , author: 'patrick rothfuss'},
+                {title: 'the way of kings' , author: 'brandon sanderson'},
+                {title: 'the final empire' , author: 'brandon sanderson'},
+            ]
         }
     },
-    medthods: {
-        toggleShowBooks() {
-            this.showBooks = !this.showbooks
-        }
+    methods: {
+        toggleShowbooks() {
+            this.showbooks = !this.showbooks
+        },
+
     }
 })
 
