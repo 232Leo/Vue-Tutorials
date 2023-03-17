33b0c61b38c4c89eea5bd8c1ef43e19d403cf1be
Leo <223lmaus@frhsd.com>
2023-03-17 09:24:25 -0400

    finished chapter 3

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ const app = Vue.createApp({
             url: 'https://www.thenetninja.co.uk',
             showbooks: 'true',
             books: [
-                {title: 'name of the wind' , author: 'patrick rothfuss', img: 'assets/1.jpg' },
-                {title: 'the way of kings' , author: 'brandon sanderson', img: 'assets/2.jpg' },
-                {title: 'the final empire' , author: 'brandon sanderson', img: 'assets/3.jpg' },
+                {title: 'name of the wind' , author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+                {title: 'the way of kings' , author: 'brandon sanderson', img: 'assets/2.jpg', isFav:  false },
+                {title: 'the final empire' , author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
             ]
         }
     },
@@ -44,7 +44,14 @@ const app = Vue.createApp({
         toggleShowbooks() {
             this.showbooks = !this.showbooks
         },
-
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
